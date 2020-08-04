de4fb17a543d86541341b7199cb5d543c530d0c1
HallanCosta <hallan.costa1@hotmail.com>
2020-08-04 17:52:11 -0300

    create migrations with knex

---
+++ server/src/database/migrations/03_create_connection.ts
@@ -0,0 +1,26 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary();
+
+    table.integer('week_day').notNullable();
+    table.integer('from').notNullable();
+    table.integer('to').notNullable();
+
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+
+    table.timestamp('created_at')
+      .defaultTo('now()')
+      .notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('connections');
+}
