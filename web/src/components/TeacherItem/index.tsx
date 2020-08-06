3df1667455b3412d853b21c317616f4c944472a6
HallanCosta <hallan.costa1@hotmail.com>
2020-08-06 00:00:46 -0300

    add props Teacher in component TeacherItem

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,32 +2,59 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/60573155?s=460&u=86cfeb9a11d851329a2f51299d3c35f552fcb0e1&v=4" alt="HállanCosta" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Hállan da Silva Costa</strong>
-          <span>Desenvolvimento Web/Mobile</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de web development samurai jedai.
-        <br />
-        Apaixonado por desenvolver aplicações que solta misseís diretamente da NASA.
-      </p>
+
+      <p>{teacher.bio}</p>
+
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
-          <img src={whatsappIcon} alt="Whatsapp" />
+
+        <a 
+          target='_blank' 
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img  src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
